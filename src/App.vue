d9e24a39a3e9d34d93b67528ff0e2e5e41e163bf
mikegulik@gmail.com <mikegulik@gmail.com>
2020-12-06 10:03:18 -0600

    node chapter component made

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+ <ChapterNode></ChapterNode>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import ChapterNode from '@/components/ChapterNode.vue'
 
 @Options({
-  components: {
-    HelloWorld
-  }
+  components: {ChapterNode},
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import './src/styles/main.scss';
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: linear-gradient(
+      252.44deg,
+      rgba(218, 218, 218, 0) 0%,
+      #fafafa 100%
+    ),
+    #f1f1f1;
+
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
+
 </style>
